ea47916c5c762ba58057b0a5383d76ff29df5000
Prince <pntombela@outlook.com>
2019-09-22 15:41:35 +0200

    'Initial_commit'

---
+++ backend/ejbModule/model/TransactionEntity.java
@@ -0,0 +1,60 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity(name="transaction_tbl")
+public class TransactionEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int TransactionCode;
+	private String name;
+	private String sourcePassport;
+	private String destinationBank;
+	private String destinationCountry;
+	private String accountNumber;
+	private Double Amount;
+	public int getTransactionCode() {
+		return TransactionCode;
+	}
+	public void setTransactionCode(int transactionCode) {
+		TransactionCode = transactionCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSourcePassport() {
+		return sourcePassport;
+	}
+	public void setSourcePassport(String sourcePassport) {
+		this.sourcePassport = sourcePassport;
+	}
+	public String getDestinationBank() {
+		return destinationBank;
+	}
+	public void setDestinationBank(String destinationBank) {
+		this.destinationBank = destinationBank;
+	}
+	public String getDestinationCountry() {
+		return destinationCountry;
+	}
+	public void setDestinationCountry(String destinationCountry) {
+		this.destinationCountry = destinationCountry;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public Double getAmount() {
+		return Amount;
+	}
+	public void setAmount(Double amount) {
+		Amount = amount;
+	}
+	
+}
